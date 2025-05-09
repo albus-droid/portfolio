e658cea0d9259de601348d685a1b471972038f94
albus-droid <albinbabuvarghese20@gmail.com>
2025-05-09 15:38:59 +0000

    Added a elisp build script

---
+++ build.el
@@ -0,0 +1,14 @@
+;; Load the publishing system
+(require 'ox-publish)
+(setq org-publish-project-alist
+      ( list
+	(list "my-portfolio"
+	      :recursive t
+	      :base-directory "."
+	      :publishing-directory "."
+	      :base-extension "org"
+	      :publishing-function 'org-html-publish-to-html)))
+
+;; Generate the site output
+(org-publish-all t)
+(message "Build Complete!")
