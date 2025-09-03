cc9aa8aa5e2dee34d89c795f6c577a4902ca1cd5
ALBIN BABU VARGHESE <albinbabuvarghese20@gmail.com>
2025-09-03 00:13:30 +0000

    rewrite build.el

--- builders/build.el
+++ builders/build.el
@@ -1,26 +1,16 @@
+;; This script builds html files from org files
+;; Load the publishing system
 (require 'ox-html)
 (require 'ox-publish)
 
-(defun slurp (path)
-  (with-temp-buffer
-    (insert-file-contents path)
-    (buffer-string)))
-
 (setq org-publish-project-alist
-      (list
-       (list "my-portfolio"
-             :recursive t
-             :base-directory "."
-             :publishing-directory "."
-             :base-extension "org"
-             :publishing-function 'org-html-publish-to-html
-             :html-head-include-default-style nil
-             :html-head-include-scripts nil
-             :html-head "<link rel=\"stylesheet\" href=\"style.css\"/>"
-             :html-head-extra "<script src=\"typewriter-name.js\" defer></script>"
-             :with-title nil
-             :html-container "main"
-             :html-preamble (slurp "layout-fragment.html"))))
+      ( list
+	(list "my-portfolio"
+	      :recursive t
+	      :base-directory "contents"
+	      :publishing-directory "dist"
+	      :base-extension "org"
+	      :publishing-function 'org-html-publish-to-html)))
 
 (org-publish-all t)
 (message "Build Complete!")
