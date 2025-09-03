ff82afa238b9da89e885a02f02fe3e54eebd01eb
ALBIN BABU VARGHESE <albinbabuvarghese20@gmail.com>
2025-09-03 00:13:30 +0000

    rewrite build.el

--- builders/build.el
+++ builders/build.el
@@ -1,12 +1,16 @@
 ;; This script builds html files from org files
 ;; Load the publishing system
+(require 'ox-html)
 (require 'ox-publish)
+
+(defun )
+
 (setq org-publish-project-alist
       ( list
 	(list "my-portfolio"
 	      :recursive t
-	      :base-directory "."
-	      :publishing-directory "."
+	      :base-directory "contents"
+	      :publishing-directory "dist"
 	      :base-extension "org"
 	      :publishing-function 'org-html-publish-to-html)))
 
